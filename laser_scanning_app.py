efeb41e3d8989bd905ee08b7890296196d18dcfa
Harishankar Jayakumar <hari@lbl.gov>
2026-04-06 15:10:35 -0700

    setup

--- laser_scanning_app.py
+++ laser_scanning_app.py
@@ -1,7 +1,27 @@
 from ScopeFoundry import BaseMicroscopeApp
+from ScopeFoundryHW.mcp_server.mcp_server_hw import MCPServerHardware
+from ScopeFoundryHW.session_manager.git_session_manager_hw import GitSessionManagerHW
+
+"""
+Harishankar Jayakumar 4/3/26 
+"""
 
 class LaserScanningApp(BaseMicroscopeApp):
-        print("Hello from laserscanning-microscope!")
+
+
+    name = 'laser_scanning_app'
+
+    def setup(self):
+
+
+        self.add_hardware_component(MCPServerHardware(self))
+
+
+        self.add_hardware(GitSessionManagerHW)
+
+
+        
+
 
 
 if __name__ == "__main__":
